81caef85c81cbc6ef9e3c30a06f479fcf2df3250
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2021-09-07 14:45:54 +0700

    add function calculator winner

--- src/index.js
+++ src/index.js
@@ -2,34 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
-  render() {
+function Square(props){
     return (
-      <button className="square" onClick={
-        ()=>{
-          this.setState({value:'X'});
-        }
-      }>
-        {this.state.value}
+      <button className="square" onClick={props.onClick}>
+        {props.value}
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares : Array(9).fill(null),
+      isX: true
+    };
+  }
   renderSquare(i) {
-    return <Square />;
+    return <Square value={this.state.squares[i]} onClick={
+      ()=> this.handleClick(i)
+    } />;
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if(calculateWinner(squares)!=null || squares[i]){
+      return;
+    }
+    squares[i] = this.state.isX ? 'X' : 'O';
+    this.setState({
+      squares:squares,
+      isX: !this.state.isX
+    });
   }
 
   render() {
-    const status = 'Next player: X';
-
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner==null){
+      status = 'Lượt tiếp theo: '+(this.state.isX ? 'X':'O');
+    }else{
+      status = winner+' thắng!';
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -69,6 +83,25 @@ class Game extends React.Component {
   }
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 // ========================================
 
 ReactDOM.render(
